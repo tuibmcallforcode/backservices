a5b3dc7375091e94397a81a4de69a2118e07cad2
mrboomba <mrboomba161@gmail.com>
2018-07-15 03:42:36 +0700

    first commit for clawler-understanding

--- src/web/routers/ibm.js
+++ src/web/routers/ibm.js
@@ -1,10 +1,27 @@
 import { analyze } from "../../controllers/ibm/nlu.js";
 import { translate } from "../../controllers/ibm/translate";
+import { readFileSync } from "fs";
+import { join } from "path"
+
+let testPath = join(__dirname, "../../../testcase/clawler/");
+let testCase = JSON.parse(readFileSync(testPath+"clawler.json"));
 
 async function nluAnalyzeHandler(ctx) {
 	const { text } = ctx.request.body;
 	let analyzed = await analyze(text);
 	ctx.body = analyzed;
+}
+
+async function nluAnalyzeClawerHandler(ctx) {
+	const body = testCase.filter((element) => element.fields.body)
+		.map((element) => element.fields.body);
+	let result = [];
+	for (let text of body) {
+		let analyzed = await analyze(text);
+		result.push(analyzed);
+	}
+	const json = JSON.stringify(result);
+	ctx.body = result;
 }
 
 async function translateHandler(ctx) {
@@ -33,6 +50,12 @@ const routes = [
 		handler: ctx => {
 			ctx.body = { r: "hello from ibm" };
 		}
+	},
+	{
+		method: "get",
+		path: "/nlu/claw",
+		middlewares: [],
+		handler: nluAnalyzeClawerHandler
 	}
 ];
 
