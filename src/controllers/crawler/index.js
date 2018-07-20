f4359f234ff1e0ceb0d0f8620035582afea3c76c
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-07-20 09:53:19 +0700

    crawler relief and pdc added

---
+++ src/controllers/crawler/index.js
@@ -0,0 +1,14 @@
+import { fetchRelieftWeb } from "./reliefWeb";
+import { fetchPDC } from "./pdc";
+// const option = {
+// 	offset: 0,
+// 	query: {
+// 		value: "earthquake"
+// 	},
+// 	profile: "list",
+// 	limit: 100,
+// 	sort: ["date:desc"]
+// };
+
+// fetchRelieftWeb(option);
+fetchPDC();
