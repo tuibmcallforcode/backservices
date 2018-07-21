3011da23eaac5d2f7ce0417f6b952b3c5e78cd59
mrboomba <mrboomba161@gmail.com>
2018-07-22 03:08:58 +0700

    add analyzed model

--- src/models/analyed.js
+++ src/models/analyed.js
@@ -2,26 +2,84 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var analyzedSchema = new Schema({
-    "usage": { "text_units": 1, "text_characters": 890, "features": 1 },
-    "semantic_roles": [
-        {
-            "subject": {
-                "text": "The Government of the Hong Kong Special Administrative Region"
-            },
-            "sentence":
-                "The Government of the Hong Kong Special Administrative Region has accepted the advice of the Disaster Relief Fund Advisory Committee and approved from the Disaster Relief Fund a grant of $3.207 million to the Adventist Development and Relief Agency, China (ADRA China) for providing relief to earthquake victims in Papua New Guinea.",
-            "object": { "text": "a grant of $3.207 million" },
-            "action": {
-                "verb": { "text": "approve", "tense": "past" },
-                "text": "approved",
-                "normalized": "approve"
-            }
-        }
-    ],
-    "language": "en"
+	usage: { text_units: number, text_characters: number, features: number },
+	semantic_roles: [
+		{
+			subject: {
+				text: String
+			},
+			sentence: String,
+			object: String,
+			action: {
+				verb: { text: String, tense: String },
+				text: String,
+				normalized: String
+			}
+		}
+	],
+	relations: [
+		{
+			type: String,
+			sentence: String,
+			score: 0.889063,
+			arguments: [
+				{
+					text: String,
+					location: [209, 248],
+					entities: [
+						{
+							type: String,
+							text: String
+						}
+					]
+				},
+				{
+					text: String,
+					location: [187, 201],
+					entities: [
+						{
+							type: String,
+							text: String,
+							disambiguation: { subtype: [String] }
+						}
+					]
+				}
+			]
+		}
+	],
+	language: String,
+	keywords: [
+		{
+			text: String,
+			sentiment: { score: Number, label: String },
+			relevance: Number,
+			emotion: {
+				sadness: Number,
+				joy: Number,
+				fear: Number,
+				disgust: Number,
+				anger: Number
+			}
+		}
+	],
+	entities: [
+		{
+			type: String,
+			text: String,
+			sentiment: { score: Number, label: String },
+			relevance: Number,
+			emotion: {
+				sadness: Number,
+				joy: Number,
+				fear: Number,
+				disgust: Number,
+				anger: Number
+			},
+			count: Number
+		}
+	],
+	categories: [{ score: Number, label: String }]
 });
-
-
 
 const Analyzed = mongoose.model("analyzed", analyzedSchema);
 module.exports = Analyzed;
